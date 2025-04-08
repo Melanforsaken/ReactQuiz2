1d8e50b715b7f615c8e8bfc2ecc6e53dda3327e5
Renzo <mailreko02@gmail.com>
2025-04-08 11:09:39 +0200

    grote wijzigingen aan ConfirmButton.js, implementatie suggesties keuzebevestiging

--- src/ConfirmButton.js
+++ src/ConfirmButton.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import './Button.css'; 
 
-const ConfirmButton = () => {
-  const handleClick = () => {
-    alert("Bevestigd!");
-  };
-
+const ConfirmButton = ({ onConfirm }) => {
   return (
-    <button className="confirm-button" onClick={handleClick}>
+    <button className="confirm-button" onClick={onConfirm}>
       bevestig keuze
     </button>
   );
