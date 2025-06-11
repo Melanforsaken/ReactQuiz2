3b1e76e016c7dc20f4a1267b2c248da85f597047
Renzo <mailreko02@gmail.com>
2025-06-11 14:06:34 +0200

    zonet een hard reset gedaan voor alles score gerelateerd, opnieuw een ScorePage.js gemaakt en de benodigde paden in App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NextPage3 from './Page3';
 import NextPage4 from './Page4';
 import NextPage5 from './Page5';
 import NextPage6 from './Page6';
+import ScorePage from './ScorePage';
 
 function App() {
   return (
@@ -34,6 +35,7 @@ function App() {
             <Route path="/fourth-page" element={<NextPage4 />} />
             <Route path="/fifth-page" element={<NextPage5 />} />
             <Route path="/sixth-page" element={<NextPage6 />} />
+            <Route path="/score" element={<ScorePage />} />
           </Routes>
         </main>
       </div>
